769b5d4ce67fed3abc914698ce48bb51e3c9a9c0
Kareem Mohamed <kareem3d.a@gmail.com>
2016-02-18 10:16:42 +0200

    Testing autoupdate of staging server

--- src/components/GoodsenseLogoComponent.tsx
+++ src/components/GoodsenseLogoComponent.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 
 export class GoodsenseLogoComponent extends React.Component<any, any> {
 	render() {
-		return <div>Goodsense Logo 1001</div>;
+		return <div>Goodsense Logo 1002</div>;
 	}
 }
