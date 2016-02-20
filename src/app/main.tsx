39bc445d9a06f4a36d4e5f16be3f04664b9ae42d
Kareem Mohamed <kareem3d.a@gmail.com>
2016-02-20 10:20:12 +0200

    T

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,6 +3,9 @@ import * as ReactDOM from 'react-dom';
 import {AppContainer} from 'src/containers/AppContainer/AppContainer';
 import './main.css';
 
+var _:any = require('lodash');
+console.log(_.last([1,2,4]));
+
 ReactDOM.render(
 	<AppContainer></AppContainer>,
 	document.getElementById('goodsense')
